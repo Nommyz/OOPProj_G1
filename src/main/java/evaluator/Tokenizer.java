f1eae192c9a7a90ffa7a31622aaa365b726ee992
Phumiphat Srikrachang <76035501+Nommyz@users.noreply.github.com>
2023-02-27 17:24:10 +0700

    fix Tokenizer gone wrong + testParser Class

---
+++ src/main/java/evaluator/Tokenizer.java
@@ -0,0 +1,57 @@
+package evaluator;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.regex.Pattern;
+
+public class Tokenizer {
+    private static final Pattern splitPattern =
+            Pattern.compile("([\\s]++)|(?<=[=+-\\-*/%(){}^])|(?=[=+\\-*/%(){}^])");
+
+    protected Queue<String> tokenlist;
+
+    public Tokenizer(String stream) {
+        tokenlist = new LinkedList<>();
+        String[] separated = splitPattern.split(stream);
+        for (String s : separated) {
+            if (!s.trim().isEmpty()) {
+                tokenlist.add(s);
+            }
+        }
+    }
+
+    public String peek() throws SyntaxError {
+        if (tokenlist.size() != 0)
+            return tokenlist.element();
+        else {
+            return "No Element";
+        }
+    }
+
+    public boolean peek(String s) throws SyntaxError {
+        return peek().equals(s);
+    }
+
+    public String consume() {
+        return tokenlist.remove();
+    }
+
+    public void consume(String s) throws SyntaxError {
+        if (peek(s)) {
+            consume();
+        } else throw new SyntaxError("Error");
+    }
+
+    public boolean empty() {
+        return tokenlist.isEmpty();
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
